663329732fa516fa9b23a62026c7b30e0a2136f1
FunnyShadow <noreply@maintest233.top>
2024-04-06 22:57:42 +0800

    Refactoring MCSManager installation function

--- setup.sh
+++ setup.sh
@@ -178,107 +178,85 @@ install_node() {
     return 0;
 }
 
+install_mcsmanager() {
+    print_log "INFO" "Installing MCSManager ...";
+    
+    # Download MCSManager
+    if ${DEBUG}; then
+        wget "${mcsmanager_download_url}" -q --progress=bar:force -c --retry-connrefused -t 5 -v -O "${tmp_path}/mcsmanager.tar.gz";
+        wget "${mcsmanager_hash_url}" -q --progress=bar:force -c --retry-connrefused -t 5 -v -O "${tmp_path}/mcsmanager.sha256";
+    else
+        wget "${mcsmanager_download_url}" -q --progress=bar:force -c --retry-connrefused -t 5 -O "${tmp_path}/mcsmanager.tar.gz";
+        wget "${mcsmanager_hash_url}" -q --progress=bar:force -c --retry-connrefused -t 5 -O "${tmp_path}/mcsmanager.sha256";
+    fi
+    
+    # Check MCSManager integrity
+    local offical_hash
+    local file_hash
+    offical_hash=$(cat "${tmp_path}/mcsmanager.sha256");
+    file_hash=$(sha256sum "${tmp_path}/mcsmanager.tar.gz" | awk '{ print $1 }');
+    if [[ "${offical_hash}" != "${file_hash}" ]]; then
+        print_log "ERROR" "MCSManager checksum failure!"
+        print_log "ERROR" "Expected: ${offical_hash}";
+        print_log "ERROR" "Actual: ${file_hash}";
+        return 1;
+    fi
+    
+    # Install MCSManager
+    if ${DEBUG}; then
+        tar -zxvf "${tmp_path}/mcsmanager.tar.gz" -C "${tmp_path}/mcsmanger";
+    else
+        tar -zxf "${tmp_path}/mcsmanager.tar.gz" -C "${tmp_path}/mcsmanger";
+    fi
+    mv -f "${tmp_path}/mcsmanger/web" "${web_install_path}";
+    mv -f "${tmp_path}/mcsmanger/daemon" "${daemon_install_path}";
+    
+    # Install dependencies
+    install_npm_packages "${web_install_path}"
+    install_npm_packages "${daemon_install_path}"
+    
+    # Set permissions
+    chmod -R 755 /opt/mcsmanager/
+    
+    # Register MCSManager services
+    create_service_file "mcsm-web.service" "MCSManager-Web" "/opt/mcsmanager/web"
+    create_service_file "mcsm-daemon.service" "MCSManager-Daemon" "/opt/mcsmanager/daemon"
+    systemctl daemon-reload
+    
+    return 0;
+}
+
+install_npm_packages() {
+    local install_path=$1
+    if cd "${install_path}"; then
+        /usr/bin/env "${node_install_path}"/bin/node "${node_install_path}"/bin/npm install --production --no-fund --no-audit >npm_install_log
+    else
+        print_log "ERROR" "Failed to change directory to ${install_path}";
+        return 1;
+    fi
+    
+    return 0;
+}
+
+create_service_file() {
+    local file_name=$1
+    local service_name=$2
+    local working_directory=$3
+    # shellcheck disable=SC2250,SC2154
+    cat << EOF > "/etc/systemd/system/${file_name}"
+[Unit]
+Description=${service_name}
 
 [Service]
-WorkingDirectory=/opt/mcsmanager/web
+WorkingDirectory=${working_directory}
 ExecStart=${node_install_path}/bin/node app.js
 ExecReload=/bin/kill -s QUIT $MAINPID
 ExecStop=/bin/kill -s QUIT $MAINPID
-Environment=\"PATH=${PATH}\"
+Environment="PATH=${PATH}"
 
 [Install]
 WantedBy=multi-user.target
-" >/etc/systemd/system/mcsm-web.service
-
-  if [ -e "/etc/systemd/system/mcsm-web.service" ]; then
-    sudo systemctl daemon-reload
-    sudo systemctl enable mcsm-daemon.service --now
-    sudo systemctl enable mcsm-web.service --now
-    echo_green "Registered!"
-  else
-    printf "\n\n"
-    echo_red "The MCSManager was successfully installed to \"/opt/mcsmanager\"."
-    echo_red "But register to the \"systemctl\" failed!\nPlease use the \"root\" account to re-run the script!"
-    exit
-  fi
-
-  sleep 2
-
-  printf "\n\n\n\n"
-
-  echo_yellow "=================================================================="
-  echo_green "Installation is complete! Welcome to the MCSManager!!!"
-  echo_yellow " "
-  echo_cyan_n "HTTP Web Service:        "
-  echo_yellow "http://<Your IP>:23333  (Browser)"
-  echo_cyan_n "Daemon Address:          "
-  echo_yellow "ws://<Your IP>:24444    (Cluster)"
-  echo_red "You must expose ports 23333 and 24444 to use the service properly on the Internet."
-  echo_yellow " "
-  echo_cyan "Usage:"
-  echo_cyan "systemctl start mcsm-{daemon,web}.service"
-  echo_cyan "systemctl stop mcsm-{daemon,web}.service"
-  echo_cyan "systemctl restart mcsm-{daemon,web}.service"
-  echo_yellow " "
-  echo_green "Official Document: https://docs.mcsmanager.com/"
-  echo_yellow "=================================================================="
+EOF
+    
+    return 0;
 }
-
-# Environmental inspection
-if [[ "$arch" == x86_64 ]]; then
-  arch=x64
-  #echo "[-] x64 architecture detected"
-elif [[ $arch == aarch64 ]]; then
-  arch=arm64
-  #echo "[-] 64-bit ARM architecture detected"
-elif [[ $arch == arm ]]; then
-  arch=armv7l
-  #echo "[-] 32-bit ARM architecture detected"
-elif [[ $arch == ppc64le ]]; then
-  arch=ppc64le
-  #echo "[-] IBM POWER architecture detected"
-elif [[ $arch == s390x ]]; then
-  arch=s390x
-  #echo "[-] IBM LinuxONE architecture detected"
-else
-  Red_Error "[x] Sorry, this architecture is not supported yet!"
-  Red_Error "[x] Please try to install manually: https://github.com/MCSManager/MCSManager#linux"
-  exit
-fi
-
-# Define the variable Node installation directory
-node_install_path="/opt/node-$node-linux-$arch"
-
-# Check network connection
-echo_cyan "[-] Architecture: $arch"
-
-# Install related software
-echo_cyan_n "[+] Installing dependent software(git,tar)... "
-if [[ -x "$(command -v yum)" ]]; then
-  sudo yum install -y git tar >error
-elif [[ -x "$(command -v apt-get)" ]]; then
-  sudo apt-get install -y git tar >error
-elif [[ -x "$(command -v pacman)" ]]; then
-  sudo pacman -Syu --noconfirm git tar >error
-elif [[ -x "$(command -v zypper)" ]]; then
-  sudo zypper --non-interactive install git tar >error
-fi
-
-# Determine whether the relevant software is installed successfully
-if [[ -x "$(command -v git)" && -x "$(command -v tar)" ]]; then
-  echo_green "Success"
-else
-  echo_red "Failed"
-  echo "$error"
-  Red_Error "[x] Related software installation failed, please install git and tar packages manually!"
-  exit
-fi
-
-# Install the Node environment
-Install_Node
-
-# Install MCSManager
-Install_MCSManager
-
-# Create MCSManager background service
-Create_Service
